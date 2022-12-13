b1cd5004568267f7d3ad8a5d3bb0abc49a821d5a
josepfpinto <josepfpinto@gmail.com>
2022-12-13 19:10:34 +0000

    cors

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from collections import namedtuple
 import json
 from random import choice
-from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+from flask import Flask, request, jsonify, make_response
+
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/", methods=["GET"])
-@cross_origin()
-def hello_world():
-    data = {'data':'Hello World!'}
-    return data, 200 # data is automatically 'jsonify' and the '200' corresponds to the status code
+
+@app.route("/", methods=["GET", "OPTIONS"])
+def api_create_order():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "GET": # The actual request following the preflight
+        data = {'data':'Hello World!'}
+        return _corsify_actual_response(jsonify(data))
+    else:
+        raise RuntimeError("Sorry, don't know how to handle method {}".format(request.method))
